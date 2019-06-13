a02a1e45cff0cecb494604dfaa4529171a191ad4
BesaFX <islamhaker@gmail.com>
2019-06-13 14:54:23 +0200

    [FIX] disable sessions

--- src/main/java/com/besafx/app/config/WebSecurityConfig.java
+++ src/main/java/com/besafx/app/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,6 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors();
+        http.httpBasic().disable();
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
@@ -53,16 +55,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler);
 
-        http.sessionManagement()
-                .maximumSessions(1)
-                .sessionRegistry(sessionRegistry());
+        // Make sure we use stateless session; session won't be used to store user's state.
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+        // Add a filter to validate the tokens with every request
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
     }
 
     @Bean
